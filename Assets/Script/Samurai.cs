10136bc7ff0ef37d6e483cc41d508698d6bc0511
XeIN <btmo4444@gmail.com>
2020-05-02 16:33:42 -0300

    slicing in half when dying

---
+++ Assets/Script/Samurai.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Samurai : MonoBehaviour
+{
+    [SerializeField] string name;
+
+    [SerializeField] GameObject topImage;
+    [SerializeField] GameObject bottomImage;
+    void Start()
+    {
+        GameManager.Instance.call_OnEndClash_Events += ReactToEndClash;
+    }
+
+    void ReactToEndClash(string winner){
+        if(winner == name){
+            Win();
+        } else {
+            Die();
+        }
+    }
+
+    void Win(){
+
+    }
+
+    void Die(){
+        topImage.GetComponent<Image>().color = Color.red;
+        bottomImage.GetComponent<Image>().color = Color.red;
+
+        topImage.GetComponent<RectTransform>().localPosition += new Vector3(38, 0, 0); 
+    }
+}
